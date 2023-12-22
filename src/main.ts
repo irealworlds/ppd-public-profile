65b8fe169f80b98e3f12f2b2d1a556a8d2d50c76
Marin Calin <marincalin2551@hotmail.com>
2023-12-22 13:26:11 +0200

    Added AOS animations.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
 import './style.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 document.title = "Marin Călin | Public Profile"
+
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    duration: 500, // values from 0 to 3000, with step 50ms
+    delay: 50, // values from 0 to 3000, with step 50ms
+});
